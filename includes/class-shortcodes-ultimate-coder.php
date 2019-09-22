93780fe2fc84bfa65c714f1106369463dc519cde
Vladimir Anokhin <ano.vladimir@gmail.com>
2019-09-22 12:25:25 +0300

    Create class-shortcodes-ultimate-coder.php

---
+++ includes/class-shortcodes-ultimate-coder.php
@@ -0,0 +1,13 @@
+<?php
+
+class Shortcodes_Ultimate_Coder {
+
+	public $plugin_prefix;
+
+	public function __construct( $plugin_prefix ) {
+		$this->plugin_prefix = $plugin_prefix;
+	}
+
+	public function display_popup() {}
+
+}
