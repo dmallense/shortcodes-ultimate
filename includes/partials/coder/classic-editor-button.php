a470cccfed63076ffb8b459b97a408481358e246
Vladimir Anokhin <ano.vladimir@gmail.com>
2019-09-26 14:02:12 +0300

    Update classic-editor-button.php

--- includes/partials/coder/classic-editor-button.php
+++ includes/partials/coder/classic-editor-button.php
@@ -4,7 +4,7 @@
 type="button"
 class="su-coder-button <?php echo esc_attr( $data['class'] ); ?>"
 title="<?php echo esc_attr( $data['title'] ); ?>"
-onclick='SUG.App.insert( "classic", { "editorID": "<?php echo esc_js( $data['editor'] ); ?>" } );'
+onclick='SUCoder.App.insertClassic( "<?php echo esc_js( $data['editor'] ); ?>" );'
 >
 <?php if ( $data['icon'] ) : ?>
 	<svg viewBox="0 0 20 20" width="14" height="14" aria-hidden="true"><path fill="currentcolor" d="M8,0l0,4l-4,0l0,12l4,0l0,4l-8,0l0,-20l8,0Zm11.972,20l-7.929,0l-0.043,-4l4,0l0,-12l-3.957,0l0,-4l7.929,0l0,20Z"/></svg>
