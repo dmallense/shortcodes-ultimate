909cbdf8fe4b4c5684333f59e51f6f859b99c5d1
Vladimir Anokhin <ano.vladimir@gmail.com>
2019-09-27 14:24:57 +0300

    Update index.js

--- includes/js/coder/src/index.js
+++ includes/js/coder/src/index.js
@@ -1,137 +1,133 @@
-var SUCoder = SUCoder || {};
+/* global jQuery, XMLHttpRequest, SUCoderL10n, SUCoderAjaxURL */
 
-SUCoder.App = (() => {
-	var self = {};
+window.SUCoder = window.SUCoder || {}
 
-	self.el = {
-		app: null,
-		search: null,
-		closeBtn: null,
-		shortcodes: null,
-		breadcrumbs: null,
-		settings: null,
-		preview: null
-	};
+window.SUCoder.App = (() => {
+  var self = {}
 
-	self.MFPOptions = null;
+  self.el = {
+    app: null,
+    search: null,
+    closeBtn: null,
+    shortcodes: null,
+    breadcrumbs: null,
+    settings: null,
+    preview: null
+  }
 
-	self.cache = {
-		shortcodes: null,
-		settings: {}
-	};
+  self.MFPOptions = null
 
-	self.state = {
-		shortcodesAdded: false
-	};
+  self.shortcodes = null
 
-	self.init = function() {
-		self.el.app = document.querySelectorAll('.su-coder-app')[0];
+  self.state = {
+    shortcodesAdded: false
+  }
 
-		if (!self.el.app) {
-			return;
-		}
+  self.init = function () {
+    self.el.app = document.querySelectorAll('.su-coder-app')[0]
 
-		self.addHeader();
-		self.addShortcodes();
-	};
+    if (!self.el.app) {
+      return
+    }
 
-	self.addHeader = function() {
-		var html = `
-			<div class="su-coder-header wp-ui-highlight">
-				<input type="text" value="" placeholder="${SUCoderL10n.searchShortcodes}" />
-				<button class="su-coder-close-btn" aria-label="${
-					SUCoderL10n.closeDialog
-				}" title="${SUCoderL10n.closeDialog}">
-					<svg viewBox="0 0 32 32" width="30" height="30" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="5" aria-hidden="true"><path d="M2 30 L30 2 M30 30 L2 2" /></svg>
-				</button>
-			</div>
-		`;
+    self.addHeader()
+    self.addShortcodes()
+  }
 
-		self.el.app.insertAdjacentHTML('afterbegin', html);
-	};
+  self.addHeader = function () {
+    var html = `
+      <div class="su-coder-header wp-ui-highlight">
+        <input type="text" value="" placeholder="${SUCoderL10n.searchShortcodes}" />
+        <button class="su-coder-close-btn" aria-label="${SUCoderL10n.closeDialog}" title="${SUCoderL10n.closeDialog}">
+        <svg viewBox="0 0 32 32" width="30" height="30" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="5" aria-hidden="true"><path d="M2 30 L30 2 M30 30 L2 2" /></svg>
+        </button>
+      </div>
+    `
 
-	self.addShortcodes = function() {
-		var html = `<div class="su-coder-shortcodes"></div>`;
+    self.el.app.insertAdjacentHTML('afterbegin', html)
+  }
 
-		self.el.app.insertAdjacentHTML('beforeend', html);
-	};
+  self.addShortcodes = function () {
+    var html = '<div class="su-coder-shortcodes"></div>'
 
-	self.loadShortcodes = function() {
-		if (!self.cache.shortcodes) {
-		}
+    self.el.app.insertAdjacentHTML('beforeend', html)
+  }
 
-		// self.cache.shortcodes = data;
+  self.loadShortcodes = function () {
+    if (!self.shortcodes) {
+    }
 
-		return self.cache.shortcodes;
-	};
+    return self.shortcodes
+  }
 
-	self.openPopup = function() {
-		if (!self.cache.shortcodes) {
-			self.fetchShortcodes();
-		}
+  self.openPopup = function () {
+    if (!self.shortcodes) {
+      self.fetchShortcodes()
+    }
 
-		self.MFPOptions = {
-			type: 'inline',
-			alignTop: true,
-			closeOnBgClick: false,
-			mainClass: 'su-coder-mfp',
-			items: {
-				src: '.su-coder-app'
-			},
-			callbacks: {}
-		};
+    self.MFPOptions = {
+      type: 'inline',
+      alignTop: true,
+      closeOnBgClick: false,
+      mainClass: 'su-coder-mfp',
+      items: {
+        src: '.su-coder-app'
+      },
+      callbacks: {}
+    }
 
-		jQuery.magnificPopup.open(self.MFPOptions);
-	};
+    jQuery.magnificPopup.open(self.MFPOptions)
+  }
 
-	self.insertClassic = function(target = '', shortcode = '') {
-		self.openPopup();
-	};
+  self.insertClassic = function (target = '', shortcode = '') {
+    self.openPopup()
+  }
 
-	self.fetch = function(method, url, params, callback) {
-		var request = new XMLHttpRequest();
+  self.fetch = function (method, url, params, callback) {
+    var request = new XMLHttpRequest()
 
-		request.open(method, url, true);
+    request.open(method, url, true)
 
-		request.setRequestHeader(
-			'Content-type',
-			'application/x-www-form-urlencoded'
-		);
+    request.setRequestHeader(
+      'Content-type',
+      'application/x-www-form-urlencoded'
+    )
 
-		request.onload = function() {
-			if (200 !== this.status) {
-				return;
-			}
+    request.onload = function () {
+      if (this.status !== 200) {
+        return
+      }
 
-			callback(this.responseText);
-		};
+      callback(this.responseText)
+    }
 
-		request.send(self.serialize(params));
-	};
+    request.send(self.serialize(params))
+  }
 
-	self.fetchShortcodes = function() {
-		self.fetch(
-			'POST',
-			SUCoderAjaxURL,
-			{ action: 'su_coder_get_shortcodes' },
-			data => {
-				self.cache.shortcode = JSON.parse(data);
-			}
-		);
-	};
+  self.fetchShortcodes = function () {
+    self.fetch(
+      'POST',
+      SUCoderAjaxURL,
+      { action: 'su_coder_get_shortcodes' },
+      data => {
+        self.shortcodes = JSON.parse(data)
+        console.log(self.shortcodes)
+      }
+    )
+  }
 
-	self.serialize = function(obj) {
-		return Object.keys(obj)
-			.map(
-				key => encodeURIComponent(key) + '=' + encodeURIComponent(obj[key])
-			)
-			.join('&');
-	};
+  self.serialize = function (obj) {
+    return Object.keys(obj)
+      .map(
+        key => encodeURIComponent(key) + '=' + encodeURIComponent(obj[key])
+      )
+      .join('&')
+  }
 
-	return {
-		init: self.init,
-		insertClassic: self.insertClassic
-	};
-})();
+  return {
+    init: self.init,
+    insertClassic: self.insertClassic
+  }
+})()
 
-document.addEventListener('DOMContentLoaded', SUCoder.App.init);
+document.addEventListener('DOMContentLoaded', window.SUCoder.App.init)
