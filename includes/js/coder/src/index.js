811c58859a68ee667fe63bb16280c148fdb8d2f0
Vladimir Anokhin <ano.vladimir@gmail.com>
2019-10-08 12:06:12 +0300

    Update index.js

--- includes/js/coder/src/index.js
+++ includes/js/coder/src/index.js
@@ -10,9 +10,22 @@ const store = {
     settings: null,
     preview: null
   },
+  state: {
+    popupReady: false
+  },
   data: {
     shortcodes: null,
     groups: null
+  },
+  MFPOptions: {
+    type: 'inline',
+    alignTop: true,
+    closeOnBgClick: false,
+    mainClass: 'su-coder-mfp',
+    items: {
+      src: '.su-coder-app'
+    },
+    callbacks: {}
   }
 }
 
@@ -23,46 +36,35 @@ const init = function () {
     return
   }
 
-  appendHeader()
-  appendShortcodes()
+  bindEvents()
 }
 
-const appendHeader = function () {
-  var html = `
+const bindEvents = function () {}
+
+const buildPopup = function () {
+  const html = `
       <div class="su-coder-header wp-ui-highlight">
         <input type="text" value="" placeholder="${SUCoderL10n.searchShortcodes}" />
         <button class="su-coder-close-btn" aria-label="${SUCoderL10n.closeDialog}" title="${SUCoderL10n.closeDialog}">
         <svg viewBox="0 0 32 32" width="30" height="30" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="5" aria-hidden="true"><path d="M2 30 L30 2 M30 30 L2 2" /></svg>
         </button>
       </div>
+      <div class="su-coder-shortcodes"></div>
     `
-
-  store.el.app.insertAdjacentHTML('afterbegin', html)
-}
-
-const appendShortcodes = function () {
-  var html = '<div class="su-coder-shortcodes"></div>'
 
   store.el.app.insertAdjacentHTML('beforeend', html)
 }
 
 const openPopup = function () {
-  const MFPOptions = {
-    type: 'inline',
-    alignTop: true,
-    closeOnBgClick: false,
-    mainClass: 'su-coder-mfp',
-    items: {
-      src: '.su-coder-app'
-    },
-    callbacks: {}
+  if (!store.state.popupReady) {
+    buildPopup()
   }
 
-  jQuery.magnificPopup.open(MFPOptions)
+  jQuery.magnificPopup.open(store.MFPOptions)
 }
 
 const fetchJSON = function (method, url, params, callback) {
-  var request = new XMLHttpRequest()
+  const request = new XMLHttpRequest()
 
   request.open(method, url, true)
 
