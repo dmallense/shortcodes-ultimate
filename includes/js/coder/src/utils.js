665dea14c94e0bb34eb6d9f067b0501b4920462f
Vladimir Anokhin <ano.vladimir@gmail.com>
2019-10-09 11:39:23 +0300

    Create utils.js

---
+++ includes/js/coder/src/utils.js
@@ -0,0 +1,38 @@
+/* global XMLHttpRequest */
+
+export function inArray (needle, haystack) {
+  var length = haystack.length
+  for (var i = 0; i < length; i++) {
+    if (haystack[i] === needle) {
+      return true
+    }
+  }
+  return false
+}
+
+export function fetchJSON (method, url, params, callback) {
+  const request = new XMLHttpRequest()
+
+  request.open(method, url, true)
+
+  request.setRequestHeader(
+    'Content-type',
+    'application/x-www-form-urlencoded'
+  )
+
+  request.onload = function () {
+    if (this.status !== 200) {
+      return
+    }
+
+    callback(this.responseText)
+  }
+
+  request.send(serializeObj(params))
+}
+
+export function serializeObj (obj) {
+  return Object.keys(obj)
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(obj[key]))
+    .join('&')
+}
