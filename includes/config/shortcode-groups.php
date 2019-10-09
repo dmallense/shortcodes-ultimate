0a1b54f23cb4353461750656845247e8aba3b8a4
Vladimir Anokhin <ano.vladimir@gmail.com>
2019-10-09 11:39:36 +0300

    Create shortcode-groups.php

---
+++ includes/config/shortcode-groups.php
@@ -0,0 +1,31 @@
+<?php
+
+return apply_filters(
+	'su/data/shortcode_groups',
+	array(
+		array(
+			'id'    => 'content',
+			'title' => __( 'Content', 'shortcodes-ultimate' ),
+		),
+		array(
+			'id'    => 'box',
+			'title' => __( 'Box', 'shortcodes-ultimate' ),
+		),
+		array(
+			'id'    => 'media',
+			'title' => __( 'Media', 'shortcodes-ultimate' ),
+		),
+		array(
+			'id'    => 'gallery',
+			'title' => __( 'Gallery', 'shortcodes-ultimate' ),
+		),
+		array(
+			'id'    => 'data',
+			'title' => __( 'Data', 'shortcodes-ultimate' ),
+		),
+		array(
+			'id'    => 'other',
+			'title' => __( 'Other', 'shortcodes-ultimate' ),
+		),
+	)
+);
