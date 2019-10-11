41dddc22b44bd98acc4ff2fa162cca7266119e83
Vladimir Anokhin <ano.vladimir@gmail.com>
2019-10-11 15:11:47 +0300

    Update data-loading.js

--- includes/js/coder/src/templates/data-loading.js
+++ includes/js/coder/src/templates/data-loading.js
@@ -1,3 +1,7 @@
 export function dataLoading (args) {
-  return `<div class="su-coder-data-loading">${args.loadingPleaseWait}&hellip;</div>`
+  return `
+    <div class="su-coder-data-loading">
+      <span class="wp-ui-text-highlight">${args.loading}&hellip;</span>
+    </div>
+  `
 }
