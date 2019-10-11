116e72e433de9114351cb4b910c9eb78056ddf97
Vladimir Anokhin <ano.vladimir@gmail.com>
2019-10-11 12:13:53 +0300

    Create app.js

---
+++ includes/js/coder/src/templates/app.js
@@ -0,0 +1,18 @@
+export function app (args) {
+  return `
+    <div style="display:none">
+      <div class="su-coder-app">
+        <div class="su-coder-header">
+          <div class="su-coder-header-inner wp-ui-highlight">
+            <input type="text" value="" placeholder="${args.searchShortcodes}" />
+            <button class="su-coder-close-btn" aria-label="${args.closeDialog}" title="${args.closeDialog}">
+            <svg viewBox="0 0 32 32" width="30" height="30" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="5" aria-hidden="true"><path d="M2 30 L30 2 M30 30 L2 2" /></svg>
+            </button>
+          </div>
+        </div>
+        <div class="su-coder-picker"></div>
+        <div class="su-coder-inserter"></div>
+      </div>
+    </div>
+  `
+}
