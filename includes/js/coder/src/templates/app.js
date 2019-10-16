2f718da383dde9f94b2031511dbec127e987b283
Vladimir Anokhin <ano.vladimir@gmail.com>
2019-10-16 16:23:43 +0300

    Update app.js

--- includes/js/coder/src/templates/app.js
+++ includes/js/coder/src/templates/app.js
@@ -1,17 +1,21 @@
-export function app (args) {
+export function app ({ searchShortcodes, closeDialog }) {
   return `
-    <div style="display:none">
+    <div class="su-coder-lightbox-bg su-coder-hidden"></div>
+    <div class="su-coder-lightbox su-coder-hidden">
       <div class="su-coder-app">
         <div class="su-coder-header">
           <div class="su-coder-header-inner wp-ui-highlight">
-            <input type="text" value="" placeholder="${args.searchShortcodes}" />
-            <button class="su-coder-close-btn" aria-label="${args.closeDialog}" title="${args.closeDialog}">
+            <input type="text" value="" placeholder="${searchShortcodes}" />
+            <button class="su-coder-close-btn" aria-label="${closeDialog}" title="${closeDialog}">
             <svg viewBox="0 0 32 32" width="30" height="30" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="5" aria-hidden="true"><path d="M2 30 L30 2 M30 30 L2 2" /></svg>
             </button>
           </div>
         </div>
         <div class="su-coder-shortcodes"></div>
-        <div class="su-coder-settings"></div>
+        <div class="su-coder-shortcode">
+          <div class="su-coder-settings"></div>
+          <div class="su-coder-preview"></div>
+        </div>
       </div>
     </div>
   `
