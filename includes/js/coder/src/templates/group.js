1948b22597170861103809494558b2ce8ca8bc69
Vladimir Anokhin <ano.vladimir@gmail.com>
2019-10-16 16:23:37 +0300

    Update group.js

--- includes/js/coder/src/templates/group.js
+++ includes/js/coder/src/templates/group.js
@@ -1,8 +1,8 @@
 export function group (args) {
   return `
-    <div class="su-coder-shortcodes-group" data-group="${args.id}">
-      <div class="su-coder-shortcodes-group-title">${args.title}</div>
-      <div class="su-coder-shortcodes-group-shortcodes"></div>
+    <div class="su-coder-group" data-group="${args.id}">
+      <div class="su-coder-group-title">${args.title}</div>
+      <div class="su-coder-group-content"></div>
     </div>
   `
 }
