3470ed48ca96eda3d5d9289cfdd6f1934631d055
Vladimir Anokhin <ano.vladimir@gmail.com>
2019-10-24 10:55:32 +0300

    Delete group.js

--- includes/js/coder/src/templates/group.js
+++
@@ -1,8 +0,0 @@
-export function group (args) {
-  return `
-    <div class="su-coder-group" data-group="${args.id}">
-      <div class="su-coder-group-title">${args.title}</div>
-      <div class="su-coder-group-content"></div>
-    </div>
-  `
-}
