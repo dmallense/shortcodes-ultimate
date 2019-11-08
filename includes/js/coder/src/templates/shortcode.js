b576ddf900c777c20139aa66d55859f89ebf5ea1
Vladimir Anokhin <ano.vladimir@gmail.com>
2019-11-08 11:01:17 +0300

    Update shortcode.js

--- includes/js/coder/src/templates/shortcode.js
+++ includes/js/coder/src/templates/shortcode.js
@@ -2,7 +2,7 @@ import { getIcon } from '../utils'
 
 export function shortcode (args) {
   return `
-    <a href="javascript:;" data-id="${args.id}" title="${args.desc}">
+    <a href="javascript:;" data-id="${args.id}" data-primary-group="${args.primaryGroup}" title="${args.desc}">
       ${getIcon(args.icon)}
       <span>${args.name}</span>
     </a>
