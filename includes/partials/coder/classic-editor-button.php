fb0b0ce213b0e4e3b9931d7e9de8e1da70939910
Vladimir Anokhin <ano.vladimir@gmail.com>
2019-11-19 10:28:34 +0300

    Update classic-editor-button.php

--- includes/partials/coder/classic-editor-button.php
+++ includes/partials/coder/classic-editor-button.php
@@ -7,7 +7,7 @@ title="<?php echo esc_attr( $data['title'] ); ?>"
 onclick='window.SUCoder.insertClassicEditor( "<?php echo esc_js( $data['editor'] ); ?>" );'
 >
 <?php if ( $data['icon'] ) : ?>
-	<svg viewBox="0 0 20 20" width="14" height="14" aria-hidden="true"><path fill="currentcolor" d="M8,0l0,4l-4,0l0,12l4,0l0,4l-8,0l0,-20l8,0Zm11.972,20l-7.929,0l-0.043,-4l4,0l0,-12l-3.957,0l0,-4l7.929,0l0,20Z"/></svg>
+	<svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 16 14" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" width="16" height="14"><path fill="currentcolor" d="m6.25 3.5h-2.625v7h2.625v3.5h-5.25v-14h5.25zm8.75-3.5v14h-5.25v-3.5h2.625v-7h-2.625v-3.5z"/></svg>
 <?php endif; ?>
 <?php if ( $data['label'] ) : ?>
 	<span><?php echo esc_html( $data['label'] ); ?></span>
