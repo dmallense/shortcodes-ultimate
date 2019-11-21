1a215d169a40d1f3b9e8c794726aa4df8a8ff863
Vladimir Anokhin <ano.vladimir@gmail.com>
2019-11-21 17:40:40 +0300

    Update groups.php

--- includes/config/groups.php
+++ includes/config/groups.php
@@ -3,12 +3,12 @@
 return apply_filters(
 	'su/data/groups',
 	array(
-		'all'     => __( 'All', 'shortcodes-ultimate' ),
-		'content' => __( 'Content', 'shortcodes-ultimate' ),
-		'box'     => __( 'Box', 'shortcodes-ultimate' ),
-		'media'   => __( 'Media', 'shortcodes-ultimate' ),
-		'gallery' => __( 'Gallery', 'shortcodes-ultimate' ),
-		'data'    => __( 'Data', 'shortcodes-ultimate' ),
-		'other'   => __( 'Other', 'shortcodes-ultimate' ),
+		'all'       => __( 'All', 'shortcodes-ultimate' ),
+		'content'   => __( 'Content', 'shortcodes-ultimate' ),
+		'box'       => __( 'Box', 'shortcodes-ultimate' ),
+		'embed'     => __( 'Embed', 'shortcodes-ultimate' ),
+		'gallery'   => __( 'Gallery', 'shortcodes-ultimate' ),
+		'developer' => __( 'Developer', 'shortcodes-ultimate' ),
+		'other'     => __( 'Other', 'shortcodes-ultimate' ),
 	)
 );
