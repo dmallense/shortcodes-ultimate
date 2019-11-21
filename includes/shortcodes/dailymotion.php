5b1e31bce1e3d5709f1fa9fe7e5619f37b82cb8c
Vladimir Anokhin <ano.vladimir@gmail.com>
2019-11-21 17:39:12 +0300

    Update dailymotion.php

--- includes/shortcodes/dailymotion.php
+++ includes/shortcodes/dailymotion.php
@@ -7,7 +7,7 @@ su_add_shortcode(
 		'icon'     => plugin_dir_url( dirname( __FILE__ ) ) . 'images/coder/shortcodes/dailymotion.svg',
 		'name'     => __( 'Dailymotion', 'shortcodes-ultimate' ),
 		'type'     => 'single',
-		'group'    => 'media',
+		'group'    => 'embed',
 		'atts'     => array(
 			'url'        => array(
 				'default' => '',
