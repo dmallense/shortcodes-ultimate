ce9c7c41516de4a4e7791388cff53bdbbebe2444
Vladimir Anokhin <ano.vladimir@gmail.com>
2019-11-29 16:23:22 +0300

    Update group.js

--- includes/js/coder/src/templates/group.js
+++ includes/js/coder/src/templates/group.js
@@ -1,3 +1,7 @@
-export function group (args) {
+export function groupList (args) {
   return `<a href="javascript:;" data-group="${args.id}" ${args.class ? ' class="' + args.class + '"' : ''}>${args.title}</a>`
+}
+
+export function groupDropdown (args) {
+  return `<option value="${args.id}" ${args.selected ? ' selected' : ''}>${args.title}</option>`
 }
