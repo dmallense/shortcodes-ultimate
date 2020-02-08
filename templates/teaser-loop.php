c371a43024a4de34bb8d0255ab02775c28ffc240
Vladimir Anokhin <ano.vladimir@gmail.com>
2020-02-08 22:37:43 +0300

    Update teaser-loop.php

--- templates/teaser-loop.php
+++ templates/teaser-loop.php
@@ -1,23 +1,17 @@
 <div class="su-posts su-posts-teaser-loop">
-	<?php
-		// Posts are found
-		if ( $posts->have_posts() ) {
-			while ( $posts->have_posts() ) :
-				$posts->the_post();
-				global $post;
-				?>
-				<div id="su-post-<?php the_ID(); ?>" class="su-post">
-					<?php if ( has_post_thumbnail() ) : ?>
-						<a class="su-post-thumbnail" href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
-					<?php endif; ?>
-					<h2 class="su-post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-				</div>
-				<?php
-			endwhile;
-		}
-		// Posts not found
-		else {
-			echo '<h4>' . __( 'Posts not found', 'shortcodes-ultimate' ) . '</h4>';
-		}
-	?>
+	<?php if ( $su_posts->have_posts() ) : ?>
+		<?php while ( $su_posts->have_posts() ) : ?>
+			<?php $su_posts->the_post(); ?>
+
+			<div id="su-post-<?php the_ID(); ?>" class="su-post">
+				<?php if ( has_post_thumbnail() ) : ?>
+					<a class="su-post-thumbnail" href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+				<?php endif; ?>
+				<h2 class="su-post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+			</div>
+
+		<?php endwhile; ?>
+	<?php else : ?>
+		<h4><?php esc_html_e( 'Posts not found', 'shortcodes-ultimate' ); ?></h4>
+	<?php endif; ?>
 </div>
