9fa4d35f270b50d8691405087618695b8b4e636d
Vladimir Anokhin <ano.vladimir@gmail.com>
2020-04-17 09:34:11 +0300

    Update lightbox-content.php

--- includes/shortcodes/lightbox-content.php
+++ includes/shortcodes/lightbox-content.php
@@ -1,93 +1,96 @@
 <?php
 
-su_add_shortcode( array(
-		'id' => 'lightbox_content',
-		'callback' => 'su_shortcode_lightbox_content',
-		'image' => su_get_plugin_url() . 'admin/images/shortcodes/lightbox_content.svg',
-		'name' => __( 'Lightbox content', 'shortcodes-ultimate' ),
-		'type' => 'wrap',
-		'group' => 'gallery',
+su_add_shortcode(
+	array(
+		'id'               => 'lightbox_content',
+		'callback'         => 'su_shortcode_lightbox_content',
+		'image'            => su_get_plugin_url() . 'admin/images/shortcodes/lightbox_content.svg',
+		'name'             => __( 'Lightbox content', 'shortcodes-ultimate' ),
+		'type'             => 'wrap',
+		'group'            => 'gallery',
 		'required_sibling' => 'lightbox',
-		'article' => 'https://getshortcodes.com/docs/lightbox/',
-		'atts' => array(
-			'id' => array(
+		'article'          => 'https://getshortcodes.com/docs/lightbox/',
+		'atts'             => array(
+			'id'         => array(
 				'default' => '',
-				'name' => __( 'ID', 'shortcodes-ultimate' ),
-				'desc' => sprintf( __( 'Enter here the ID from Content source field. %s Example value: %s', 'shortcodes-ultimate' ), '<br>', '<b%value>my-custom-popup</b>' )
+				'name'    => __( 'ID', 'shortcodes-ultimate' ),
+				'desc'    => sprintf( __( 'Enter here the ID from Content source field. %1$s Example value: %2$s', 'shortcodes-ultimate' ), '<br>', '<b%value>my-custom-popup</b>' ),
 			),
-			'width' => array(
+			'width'      => array(
 				'default' => '50%',
-				'name' => __( 'Width', 'shortcodes-ultimate' ),
-				'desc' => sprintf( __( 'Adjust the width for inline content (in pixels or percents). %s Example values: %s, %s, %s', 'shortcodes-ultimate' ), '<br>', '<b%value>300px</b>', '<b%value>600px</b>', '<b%value>90%</b>' )
+				'name'    => __( 'Width', 'shortcodes-ultimate' ),
+				'desc'    => sprintf( __( 'Adjust the width for inline content (in pixels or percents). %1$s Example values: %2$s, %3$s, %4$s', 'shortcodes-ultimate' ), '<br>', '<b%value>300px</b>', '<b%value>600px</b>', '<b%value>90%</b>' ),
 			),
-			'margin' => array(
-				'type' => 'slider',
-				'min' => 0,
-				'max' => 600,
-				'step' => 5,
+			'margin'     => array(
+				'type'    => 'slider',
+				'min'     => 0,
+				'max'     => 600,
+				'step'    => 5,
 				'default' => 40,
-				'name' => __( 'Margin', 'shortcodes-ultimate' ),
-				'desc' => __( 'Adjust the margin for inline content (in pixels)', 'shortcodes-ultimate' )
+				'name'    => __( 'Margin', 'shortcodes-ultimate' ),
+				'desc'    => __( 'Adjust the margin for inline content (in pixels)', 'shortcodes-ultimate' ),
 			),
-			'padding' => array(
-				'type' => 'slider',
-				'min' => 0,
-				'max' => 600,
-				'step' => 5,
+			'padding'    => array(
+				'type'    => 'slider',
+				'min'     => 0,
+				'max'     => 600,
+				'step'    => 5,
 				'default' => 40,
-				'name' => __( 'Padding', 'shortcodes-ultimate' ),
-				'desc' => __( 'Adjust the padding for inline content (in pixels)', 'shortcodes-ultimate' )
+				'name'    => __( 'Padding', 'shortcodes-ultimate' ),
+				'desc'    => __( 'Adjust the padding for inline content (in pixels)', 'shortcodes-ultimate' ),
 			),
 			'text_align' => array(
-				'type' => 'select',
-				'values' => array(
+				'type'    => 'select',
+				'values'  => array(
 					'left'   => __( 'Left', 'shortcodes-ultimate' ),
 					'center' => __( 'Center', 'shortcodes-ultimate' ),
-					'right'  => __( 'Right', 'shortcodes-ultimate' )
+					'right'  => __( 'Right', 'shortcodes-ultimate' ),
 				),
 				'default' => 'center',
-				'name' => __( 'Text alignment', 'shortcodes-ultimate' ),
-				'desc' => __( 'Select the text alignment', 'shortcodes-ultimate' )
+				'name'    => __( 'Text alignment', 'shortcodes-ultimate' ),
+				'desc'    => __( 'Select the text alignment', 'shortcodes-ultimate' ),
 			),
 			'background' => array(
-				'type' => 'color',
+				'type'    => 'color',
 				'default' => '#FFFFFF',
-				'name' => __( 'Background color', 'shortcodes-ultimate' ),
-				'desc' => __( 'Pick a background color', 'shortcodes-ultimate' )
+				'name'    => __( 'Background color', 'shortcodes-ultimate' ),
+				'desc'    => __( 'Pick a background color', 'shortcodes-ultimate' ),
 			),
-			'color' => array(
-				'type' => 'color',
+			'color'      => array(
+				'type'    => 'color',
 				'default' => '#333333',
-				'name' => __( 'Text color', 'shortcodes-ultimate' ),
-				'desc' => __( 'Pick a text color', 'shortcodes-ultimate' )
+				'name'    => __( 'Text color', 'shortcodes-ultimate' ),
+				'desc'    => __( 'Pick a text color', 'shortcodes-ultimate' ),
 			),
-			'color' => array(
-				'type' => 'color',
+			'color'      => array(
+				'type'    => 'color',
 				'default' => '#333333',
-				'name' => __( 'Text color', 'shortcodes-ultimate' ),
-				'desc' => __( 'Pick a text color', 'shortcodes-ultimate' )
+				'name'    => __( 'Text color', 'shortcodes-ultimate' ),
+				'desc'    => __( 'Pick a text color', 'shortcodes-ultimate' ),
 			),
-			'shadow' => array(
-				'type' => 'shadow',
+			'shadow'     => array(
+				'type'    => 'shadow',
 				'default' => '0px 0px 15px #333333',
-				'name' => __( 'Shadow', 'shortcodes-ultimate' ),
-				'desc' => __( 'Adjust the shadow for content box', 'shortcodes-ultimate' )
+				'name'    => __( 'Shadow', 'shortcodes-ultimate' ),
+				'desc'    => __( 'Adjust the shadow for content box', 'shortcodes-ultimate' ),
 			),
-			'class' => array(
-				'type' => 'extra_css_class',
-				'name' => __( 'Extra CSS class', 'shortcodes-ultimate' ),
-				'desc' => __( 'Additional CSS class name(s) separated by space(s)', 'shortcodes-ultimate' ),
+			'class'      => array(
+				'type'    => 'extra_css_class',
+				'name'    => __( 'Extra CSS class', 'shortcodes-ultimate' ),
+				'desc'    => __( 'Additional CSS class name(s) separated by space(s)', 'shortcodes-ultimate' ),
 				'default' => '',
 			),
 		),
-		'content' => __( 'Inline content', 'shortcodes-ultimate' ),
-		'desc' => __( 'Inline content for lightbox', 'shortcodes-ultimate' ),
-		'icon' => 'external-link',
-	) );
+		'content'          => __( 'Inline content', 'shortcodes-ultimate' ),
+		'desc'             => __( 'Inline content for lightbox', 'shortcodes-ultimate' ),
+		'icon'             => 'external-link',
+	)
+);
 
 function su_shortcode_lightbox_content( $atts = null, $content = null ) {
 
-	$atts = shortcode_atts( array(
+	$atts = shortcode_atts(
+		array(
 			'id'         => '',
 			'width'      => '50%',
 			'margin'     => '40',
@@ -96,8 +99,11 @@ function su_shortcode_lightbox_content( $atts = null, $content = null ) {
 			'background' => '#FFFFFF',
 			'color'      => '#333333',
 			'shadow'     => '0px 0px 15px #333333',
-			'class'      => ''
-		), $atts, 'lightbox_content' );
+			'class'      => '',
+		),
+		$atts,
+		'lightbox_content'
+	);
 
 	su_query_asset( 'css', 'su-shortcodes' );
 
